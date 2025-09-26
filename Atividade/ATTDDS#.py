84d3faa2e2fc091a634c8107bdad39b21b67b745
Vitor <vitormateushd123@gmail.com>
2025-09-26 16:40:32 -0300

    Adicionando as funcoes RemoverPorId e ProcurarPorId.
    nada

---
+++ Atividade/ATTDDS#.py
@@ -0,0 +1,9 @@
+def RemoverPorId():
+    id = input("Digite o id do aluno que deseja remover: ")
+    
+    """XXXXXXX(id)"""
+    
+def ProcurarPorId():
+    id = input("Digite o id do aluno que deseja buscar: ")
+
+    """XXXXXXX(id)"""
